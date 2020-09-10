e1b1aff29b40098d75a5cf5455692bae49df0ea5
TheAloneShadow <43110655+thealoneshadow@users.noreply.github.com>
2020-09-10 11:34:37 +0530

    Add files via upload

---
+++ src/Checkout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Laptops/Microsoft/COOP/17_06_2020/V209073725_IN_PC_Microsoft_COOP_refreshing_Baneers_1500x200_2.jpg"
+          alt=""
+        />
+        <div className="checkout__title">
+          <h2>Your Shopping Basket</h2>
+          {/* Basket Item*/}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_aRU-bdofXzg78W48UDHu5PiEh9XfpDs",
+    authDomain: "clone-b3f97.firebaseapp.com",
+    databaseURL: "https://clone-b3f97.firebaseio.com",
+    projectId: "clone-b3f97",
+    storageBucket: "clone-b3f97.appspot.com",
+    messagingSenderId: "889926772485",
+    appId: "1:889926772485:web:d89e1d72cadf360067f96c",
+    measurementId: "G-V7ER4W61JB"
+  };
